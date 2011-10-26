435708cd564c1cc12b144b1c8f6a0fe1d2400261
Niels Meyering <nmeyerin@uos.de>
2011-10-26 17:03:13 +0200

    less spammy output

--- to_relative.py
+++ to_relative.py
@@ -9,35 +9,37 @@ def main():
 	for roots, dirs, files in os.walk(include_path):
 		for job in files:
 			job = os.path.join(roots, job)
+
+			if not re.match(r'.*\.[ch]pp$', job):
+				continue
 			print('--- Processing {}'.format(job))
 
-			if re.match(r'.*\.[ch]pp$', job):
-				with open(job, 'r') as f:
-					lines = f.readlines()
-					out_lines = []
-					for line in lines:
-						m = include_pattern.match(line)
-						if m:
-							path = m.group('path')
-							newpath = os.path.relpath(
-									os.path.normpath(
-										os.path.join(
-											os.path.dirname(os.path.abspath(job)),
-											path)),
-									include_path)
-							line = '#include <{}>\n'.format(newpath)
-							print(
+			with open(job, 'r') as f:
+				lines = f.readlines()
+				out_lines = []
+				for line in lines:
+					m = include_pattern.match(line)
+					if m:
+						path = m.group('path')
+						newpath = os.path.relpath(
+								os.path.normpath(
+									os.path.join(
+										os.path.dirname(os.path.abspath(job)),
+										path)),
+								include_path)
+						line = '#include <{}>\n'.format(newpath)
+						print(
 							'"{}"\t=> <{}>'.format(
-								path,
-								newpath))
-						out_lines.append(line)
-				try:
-					if sys.argv[1] == '-x':
-						with open(job, 'w') as f:
-							for line in out_lines:
-								f.write(line)
-				except IndexError:
-					pass
+							path,
+							newpath))
+					out_lines.append(line)
+			try:
+				if sys.argv[1] == '-x':
+					with open(job, 'w') as f:
+						for line in out_lines:
+							f.write(line)
+			except IndexError:
+				pass
 			print()
 
 if __name__ == '__main__':
